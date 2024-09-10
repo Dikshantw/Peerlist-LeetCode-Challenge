182d50ee839858151364b9bbfe707f3731a5daf2
Dikshantw <dikshantw11@gmail.com>
2024-09-10 17:57:37 +0530

    submission for day 4

---
+++ Day4/index.js
@@ -0,0 +1,19 @@
+//Problem = https://leetcode.com/problems/product-of-array-except-self/
+
+var productExceptSelf = function (nums) {
+  let prefix = new Array(nums.length).fill(1);
+  let suffix = new Array(nums.length).fill(1);
+  for (let i = 1; i < nums.length; i++) {
+    prefix[i] = prefix[i - 1] * nums[i - 1];
+  }
+  for (let i = nums.length - 2; i >= 0; i--) {
+    suffix[i] = suffix[i + 1] * nums[i + 1];
+  }
+  let answer = [];
+  for (let i = 0; i < nums.length; i++) {
+    answer[i] = prefix[i] * suffix[i];
+  }
+  return answer;
+};
+
+///////// SC can be optimised here //////////
