40720d39b7e04d2d23bcb8c7f2b6c4c711602a2e
Dikshantw <dikshantw11@gmail.com>
2024-09-13 19:04:22 +0530

    submission for day 6

---
+++ Day6/index.js
@@ -0,0 +1,15 @@
+//Problem = https://leetcode.com/problems/group-anagrams/
+
+var groupAnagrams = function (strs) {
+  let ans = {};
+
+  for (let s of strs) {
+    let key = s.split("").sort().join("");
+    if (!ans[key]) {
+      ans[key] = [];
+    }
+    ans[key].push(s);
+  }
+
+  return Object.values(ans);
+};
