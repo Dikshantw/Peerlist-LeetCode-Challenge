4363ece9388cdf242eba7aac6ff04006a25a8488
Dikshantw <dikshantw11@gmail.com>
2024-09-14 16:56:06 +0530

    submission for day 7

---
+++ Day7/index.js
@@ -0,0 +1,18 @@
+//Problem = https://leetcode.com/problems/sort-colors/
+
+var sortColors = function (nums) {
+  let count = { 0: 0, 1: 0, 2: 0 };
+
+  for (let i = 0; i < nums.length; i++) {
+    count[nums[i]]++;
+  }
+
+  let idx = 0;
+  for (let color = 0; color < 3; color++) {
+    let freq = count[color];
+    for (let j = 0; j < freq; j++) {
+      nums[idx] = color;
+      idx++;
+    }
+  }
+};
