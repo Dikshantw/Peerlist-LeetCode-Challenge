61b3493fe59e7b4004b3c3f37d62c130d840de50
Dikshantw <dikshantw11@gmail.com>
2024-09-24 15:58:43 +0530

    submission for day 17

---
+++ Day17/index.js
@@ -0,0 +1,15 @@
+//Problem = https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+
+var lowestCommonAncestor = function(root, p, q) {
+    if (root === null || root === p || root === q) {
+        return root;
+    }
+    
+    let left = lowestCommonAncestor(root.left, p, q);
+    let right = lowestCommonAncestor(root.right, p, q);
+    
+    if (left !== null && right !== null) {
+        return root;
+    }
+    return left !== null ? left : right;
+};
